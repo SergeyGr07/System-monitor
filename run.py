d87b6dfb19e6a5df9ae178ed6bff1791330f1587
Sergey Grudinin <sgrudinin2016@yandex.ru>
2025-01-10 18:12:10 +0800

    init_project

---
+++ run.py
@@ -0,0 +1,9 @@
+from app import create_app
+import webview
+
+app = create_app()
+
+if __name__ == "__main__":
+    # Запускаем Flask в отдельном потоке
+    webview.create_window("System Monitor", "http://localhost:5000")
+    app.run(port=5000)
